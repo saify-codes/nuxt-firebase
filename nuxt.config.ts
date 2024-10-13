fd1099074768593bb7b549dc50a6bb042b72e8ea
saify <sarfaraz.symits@gmail.com>
2024-10-13 06:09:35 +0500

    added ui library (primevue)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primevue/themes/aura";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/global.css'],
-  modules: ['@pinia/nuxt'],
-})
+  css: [
+    "~/assets/css/global.css",
+    'primeicons/primeicons.css',
+  ],
+  modules: [
+    "@pinia/nuxt",
+    "@primevue/nuxt-module",
+    "@nuxtjs/tailwindcss",
+  ],
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
